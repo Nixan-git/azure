d20f73130638386a9ef31b9a2842031d7db31d6e
Nikolay <nik_97@abv.bg>
2025-12-12 12:01:27 +0200

    edited RG PS command

--- Get-ResourceGroups.ps1
+++ Get-ResourceGroups.ps1
@@ -1,3 +1,10 @@
-Testing push
-2nd branch
-test 3rd branch
+# Login to Azure (interactive prompt)
+Connect-AzAccount
+
+# Get all resource groups in the subscription
+$resourceGroups = Get-AzResourceGroup
+
+# Loop through the results and output the info
+foreach ($rg in $resourceGroups) {
+    Write-Output "Resource Group: $($rg.ResourceGroupName) - Location: $($rg.Location)"
+}
